8b2ec0ec8859bef1929ca12ef8bfcdd823f96ee5
michellecamposc <stephaniecampoos95@gmail.com>
2022-10-12 17:56:10 -0400

    Funciones para leer documentos

---
+++ src/index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+//Módulos y librerias para utilizar y leer documentos
+const fileSystem = require("fs");
+const path = require("path");
+const colors = require("colors");
+
+//Lector de las rutas
+const userPath = process.argv[2]; // Muestra la ruta ingresada por el usuario
+const options = process.argv[3];
+console.log("Esta esta la ruta que dió el usuario".blue, userPath);
+
+//Convierte una ruta relativa a absoluta
+const absolutePath =
+  path.isAbsolute(userPath) === true ? userPath : path.resolve(userPath);
+console.log("Ruta relativa transformada".yellow, absolutePath);
+
+//Verificar si la ruta es un directorio o archivo
+const directoryOrFile = () => {
+  fileSystem.stat(absolutePath, (err, stats) => {
+    const file = stats.isFile(absolutePath);
+    const directory = stats.isDirectory(absolutePath);
+    if (file) {
+      return console.log("Esta ruta es un archivo".yellow + " " + file);
+    } else if (directory) {
+      return console.log(
+        "Esta ruta es un directorio ".yellow + " " + directory
+      );
+    }
+  });
+};
+directoryOrFile();
+
+//Extensión del archivo
+const fileExtension = () => {
+  return path.extname(absolutePath) === ".md";
+};
+console.log("La extensión del archivo es md".yellow, fileExtension());
+
+//Si la ruta es un directorio leer y mostrar sus archivos
+const readDirectoryFiles = () => {
+  return new Promise((resolve, reject) => {
+    const arrFiles = [];
+    fileSystem.readdir(userPath, "utf8", (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      files.forEach((file) => {
+        if (path.extname(file) === ".md") {
+          console.log("Archivos .md encontrado".rainbow, file);
+          arrFiles.push(files);
+        }
+      });
+      resolve("Archivos .md", arrFiles);
+    });
+  });
+};
+readDirectoryFiles();
+
+//Leer los archivos
+const readFiles = () => {
+  fs.readFile(absolutePath, "utf-8", (err, data) => {
+    if (err) {
+      console.log("error: ", err);
+    } else {
+      console.log(data);
+    }
+  });
+};
---
+++ src/link.md
@@ -0,0 +1,6 @@
+## 1. Texto de ejemplo
+Esto es un archivo que sirve como ejemplo para ser leído, momento de prueba.
+- [Markdown](https://es.wikipedia.org/wiki/Markdown) 
+- [Node.js](https://nodejs.org/)
+
+
